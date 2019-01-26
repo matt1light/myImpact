ac3bb23b574aaf79960fe566a47b9feec1b453ac
matt1light <matt1light@gmail.com>
2019-01-26 15:14:48 -0500

    add views and routes to those views for restAPI

--- WebServer/restAPI/urls.py
+++ WebServer/restAPI/urls.py
@@ -1,1 +1,13 @@
 # define our URLs here
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import DeviceViewSet, RateViewSet, RawDataViewSet, ServiceViewSet
+
+
+urlpatterns = [
+    url(r'^devices/', DeviceViewSet),
+    url(r'^services/', ServiceViewSet),
+    url(r'^rates/', RateViewSet),
+    url(r'^raw_datas/', RawDataViewSet),
+]
--- WebServer/restAPI/views.py
+++ WebServer/restAPI/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import ListView
 from .models import Service, Device, RawData, Rate
+from .serializer import DeviceSerializer, ServiceSerializer, RawDataSerializer, RateSerializer
 
 # Create your views here.
+
+class DeviceViewSet():
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class ServiceViewSet():
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class RawDataViewSet():
+    queryset = RawData.objects.all()
+    serializer_class = RawDataSerializer
+
+class RateViewSet():
+    queryset = Rate.objects.all()
+    serializer_class = RateSerializer
