1735ea760b9fe596816da53cdd7579d58d58a309
matt1light <matt1light@gmail.com>
2019-01-26 15:13:02 -0500

    install restAPI app

--- WebServer/WebServer/settings.py
+++ WebServer/WebServer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'restAPI.apps.RestapiConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
