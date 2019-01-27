faa0a9ef4090216570da9986f7b4d03c687aefb3
ConnorMacKenzie <connormack97@gmail.com>
2019-01-26 21:40:09 -0500

    i forgot this file

--- pub_script.py
+++ pub_script.py
@@ -2,4 +2,4 @@ import paho.mqtt.publish as publish
 
 data = '{"deviceType": "trash", "boardId": "1A2B3C", "data": " /d/d 12.32 kg"}'
 print('Sending ' + data + ' to topic rawData')
-publish.single("rawData", data, hostname="localhost")
+publish.single("rawData", data, hostname="172.30.189.189")
