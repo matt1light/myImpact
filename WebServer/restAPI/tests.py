8e6a5f125306b8f6766231f0d2b455d0c7ca64e5
Ryan Frohar <ryan.frohar@gmail.com>
2019-01-27 06:59:17 -0500

    analyze.py modified. Passed unit testing

--- WebServer/restAPI/tests.py
+++ WebServer/restAPI/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import RawData, Rate
-from .analyze import Trash
+from .analyze import Gas
 import datetime
 
 # Create your tests here.
@@ -9,18 +9,19 @@ class AnalyzerTests(TestCase):
     def setUp(self):
         today=datetime.datetime.now()
         self.raw_data_array = []
-        for x in range(0, 10):
-            data = "{\d\d " + str(x) +" kg}"
+        for x in range(0, 23):
+            data ='{"value":' + str(x*10) + ', "unit": "MJ"}'
+
             self.raw_data_array.append(
                 RawData(
-                    dateTime= today.replace(day=today.day-10 + x),
-                    deviceType= "trash",
+                    dateTime= today.replace(hour=0 + x),
+                    deviceType= "gas",
                     data = data,
                     boardId= "X1D54"))
 
     def testZeroToTen(self):
-         trash_analyzer = Trash()
-         rate = trash_analyzer.analyze(self.raw_data_array)
+         gas_analyzer = Gas()
+         rate = gas_analyzer.analyze(self.raw_data_array)
          print(rate.time)
          print(rate.units)
          print(rate.device)
